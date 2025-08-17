fae4b86e582ac10068568e360bd717a54c68f557
Mohadeseh Alijani <m.alijani@FSP.Local>
2025-08-17 17:28:40 +0330

    sun office

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -73,7 +74,7 @@ class Home extends StatelessWidget {
           const SizedBox(width: 8),
         ],
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(28.0),
         child: Column(
           children: [
@@ -121,8 +122,141 @@ class Home extends StatelessWidget {
                 ),
               ),
             ),
+            SizedBox(
+              width: double.infinity,
+              height: 350,
+              child: ListView.separated(
+                physics: BouncingScrollPhysics(),
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int position) {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                    child: MyItem(),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  if (index % 9 == 0) {
+                    return Ads();
+                  } else {
+                    return SizedBox.shrink();
+                  }
+                },
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 232, 232, 232),
+                borderRadius: BorderRadius.circular(1000),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: 50,
+                    child: TextButton.icon(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(
+                          Color.fromARGB(255, 202, 193, 255),
+                        ),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(1000),
+                          ),
+                        ),
+                      ),
+                      onPressed: () => _showSnackBar(
+                        context,
+                        "به روز رسانی با موفقیت انجام شد!",
+                      ),
+                      icon: const Icon(
+                        Icons.refresh,
+                        color: Colors.black,
+                        size: 24,
+                      ),
+                      //update button box
+                      label: Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                        child: Text(
+                          "به روز رسانی",
+                          style: Theme.of(context).textTheme.displayLarge,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                    child: Text("آخرین به روز رسانی: ${_getTime()}"),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  String _getTime() {
+    return "20:45";
+  }
+}
+
+void _showSnackBar(BuildContext context, String msg) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(msg, style: Theme.of(context).textTheme.bodyLarge),
+      backgroundColor: Colors.white,
+    ),
+  );
+}
+
+class MyItem extends StatelessWidget {
+  const MyItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      decoration: BoxDecoration(
+        boxShadow: <BoxShadow>[BoxShadow(blurRadius: 1.0, color: Colors.grey)],
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(1000),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text("دلار", style: Theme.of(context).textTheme.bodyLarge),
+
+          Text("90000", style: Theme.of(context).textTheme.bodyLarge),
+
+          Text("+3", style: Theme.of(context).textTheme.bodyLarge),
+        ],
+      ),
+    );
+  }
+}
+
+class Ads extends StatelessWidget {
+  const Ads({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      decoration: BoxDecoration(
+        boxShadow: <BoxShadow>[BoxShadow(blurRadius: 1.0, color: Colors.grey)],
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(1000),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text("تبلیغات", style: Theme.of(context).textTheme.bodyMedium),
+        ],
       ),
     );
   }
